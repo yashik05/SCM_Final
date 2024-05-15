0ede1779634af0c198245cb42431f2a352613cea
HyperBeastt <155317512+HyperBeastt@users.noreply.github.com>
2024-05-15 10:16:15 +0530

    Add files via upload

---
+++ FEE Project/test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Student{
+    String name;
+    int rollno;
+    Student(SRtring name, int rollno,){
+        this.name=namr;
+        this.rollno=rollno;
+    }
+}
+
+class display{
+
+}
+
+public static void main (String args[]){
+    Student s=new Student;
+        
+
+}
